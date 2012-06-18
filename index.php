fb501fe8eac436ffe920a122f5eceae4fe3d5ea5
Michiel Sikkes <michiel@firmhouse.com>
2012-06-18 21:03:22 +0200

    Basic theme skeleton set up!

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 <head>
 
   <title>My Awesome Git Theme</title>
+  
+  <link href="<?php echo get_template_directory_uri() ?>/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="<?php echo get_template_directory_uri() ?>/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+  <!--[if IE]>
+      <link href="<?php echo get_template_directory_uri() ?>/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
 
 </head>
 
 <body>
 
+  <div class="container">
+  
+    <div class="content">
+      <div class="posts">
+        <h1>Hello, I am content</h1>
+      </div>
+    </div>
+    
+    <div class="sidebar">
+      <div class="widget">
+        <p>Hi. Sidebar.</p>
+      </div>
+    </div>
+    
+  </div>
 
 </body>
 
